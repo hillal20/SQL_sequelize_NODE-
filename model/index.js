3bc83271721a6d06bf778bf7a7ba2c2d13a7969d
hilal aissani <hilalaissani@gmail.com>
2019-02-22 20:48:16 -0500

    2

--- model/index.js
+++ model/index.js
@@ -1,8 +1,15 @@
 const fs = require("fs");
-const sequelize = require("sequelize");
+const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 if (config.use_env_variable) {
+  const sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password
+  );
 }
