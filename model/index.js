a98fec928a2a01b5a06f7205ea16a6e021ecdb63
hilal aissani <hilalaissani@gmail.com>
2019-02-22 21:01:22 -0500

    2

--- model/index.js
+++ model/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const path = require("path");
 const Sequelize = require("sequelize");
+
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
