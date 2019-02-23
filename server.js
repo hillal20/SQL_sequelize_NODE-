5262b31a5e89e524407c3d35a6dfdaa54fbd40a9
hilal aissani <hilalaissani@gmail.com>
2019-02-23 18:52:56 -0500

    2

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const server = express();
+const Sequelize = require("sequelize");
 
+const connection = new Sequelize("db", "user", "pass", {
+  host: "localhost",
+  dialect: "sqlite",
+  storage: "db.sqlite",
+  operatorsAliases: false
+});
+
+connection
+  .authenticate()
+  .then(msg => {
+    console.log("=== database is connected===");
+  })
+  .catch(err => {
+    console.log(" === err in database connection ===");
+  });
 server.listen(9999, () => {
   console.log("=== server on 9999 ===");
 });
