d03cad553091d25e22213ed0d946652188f3f40e
hilal aissani <hilalaissani@gmail.com>
2019-03-02 18:33:21 -0500

    users

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 const Sequelize = require("sequelize");
 const { connectionFn } = require("./createUsers");
-
+const _USERS = require("./data.json");
 const connection = new Sequelize("db", "user", "pass", {
   host: "localhost",
   dialect: "sqlite",
@@ -24,60 +24,60 @@ connection
     console.log(" === err in database connection ===");
   });
 
-connectionFn(connection);
-/////////////////////////////////// models
+//connectionFn(connection);
+/////////////////////////////////// model.create
 
-const UserModel = connection.define(
-  "UserModel",
-  {
-    uuid: {
-      type: Sequelize.UUID,
-      primaryKey: true,
-      defaultValue: Sequelize.UUIDV4
-    },
-    name: {
-      type: Sequelize.STRING,
-      validate: {
-        len: [3, 9] // 3 min char and 7 max char
-      }
-    },
-    bio: Sequelize.TEXT,
-    address: Sequelize.TEXT,
-    zipCode: Sequelize.INTEGER,
-    fullAdd: Sequelize.STRING
-  },
-  {
-    hooks: {
-      beforeValidate: () => {
-        console.log("== before validate ");
-      },
-      afterValidate: () => {
-        console.log("== after validate ");
-      },
-      beforeCreate: userModel => {
-        userModel.fullAdd = `${userModel.address} ${userModel.zipCode}`;
-        console.log("== before create ");
-      },
-      afterCreate: () => {
-        console.log("== after create ");
-      }
-    }
-  }
-);
+// const UserModel = connection.define(
+//   "UserModel",
+//   {
+//     uuid: {
+//       type: Sequelize.UUID,
+//       primaryKey: true,
+//       defaultValue: Sequelize.UUIDV4
+//     },
+//     name: {
+//       type: Sequelize.STRING,
+//       validate: {
+//         len: [3, 9] // 3 min char and 7 max char
+//       }
+//     },
+//     bio: Sequelize.TEXT,
+//     address: Sequelize.TEXT,
+//     zipCode: Sequelize.INTEGER,
+//     fullAdd: Sequelize.STRING
+//   },
+//   {
+//     hooks: {
+//       beforeValidate: () => {
+//         console.log("== before validate ");
+//       },
+//       afterValidate: () => {
+//         console.log("== after validate ");
+//       },
+//       beforeCreate: userModel => {
+//         userModel.fullAdd = `${userModel.address} ${userModel.zipCode}`;
+//         console.log("== before create ");
+//       },
+//       afterCreate: () => {
+//         console.log("== after create ");
+//       }
+//     }
+//   }
+// );
 
-connection
-  .sync({ force: true })
-  .then(() => {
-    UserModel.create({
-      name: "billal",
-      bio: "hahahahah",
-      address: "settara jijel",
-      zipCode: 18340
-    });
-  })
-  .catch(err => {
-    console.log("==>", err);
-  });
+// connection
+//   .sync({ force: true })
+//   .then(() => {
+//     UserModel.create({
+//       name: "billal",
+//       bio: "hahahahah",
+//       address: "settara jijel",
+//       zipCode: 18340
+//     });
+//   })
+//   .catch(err => {
+//     console.log("==>", err);
+//   });
 
 ///////////////////////////// routes
 server.get("/user", (req, res) => {
@@ -94,6 +94,21 @@ server.get("/user", (req, res) => {
       res.json({ msg: "short name" });
     });
 });
+/////////////// model.bulkCreate
+const UserModel2 = connection.define("UserModel2", {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING
+});
+
+connection
+  .sync({ force: true })
+  .then(() => {
+    UserModel2.bulkCreate(_USERS);
+  })
+  .catch(err => {
+    console.log("==>", err);
+  });
 
 ///////////////////////////
 server.listen(9999, () => {
